b68cde229f001ee1159eb13e2c91afe6c7aedffb
MCSuda <suder.michal97@gmail.com>
2021-08-25 16:43:25 +0200

    Command to roll dice changed

--- main.py
+++ main.py
@@ -12,13 +12,54 @@ bot = commands.Bot(command_prefix="!")
 
 # Command to roll the dice
 
-# Need to work on better way to call the command (userfriendly)
 @bot.command()
-async def d(ctx, dice, howManyRolls="", modifier="", advantage=""):
+async def d(ctx, die):
+    i = 0
+    dice = ""
+    howManyRolls = ""
+    modifier = ""
+    modifierTest = True
+    advantageTest = 0
+    while i < len(die):
+        if die[i] == "d":
+            i += 1
+            while i < len(die):
+                if die[i] == "+":
+                    modifierTest = True
+                    i += 1
+                    while i < len(die):
+                        if die[i] == "A" or die[i] == "a":
+                            advantageTest = 1
+                        elif die[i] == "D" or die[i] == "d":
+                            advantageTest = 2
+                        else:
+                            modifier = modifier + die[i]
+                        i += 1
+                elif die[i] == "-":
+                    modifierTest = False
+                    i += 1
+                    while i < len(die):
+                        if die[i] == "A" or die[i] == "a":
+                            advantageTest = 1
+                        elif die[i] == "D" or die[i] == "d":
+                            advantageTest = 2
+                        else:
+                            modifier = modifier + die[i]
+                        i += 1
+
+                else:
+                    dice = dice + die[i]
+                i += 1
+        else:
+            howManyRolls = howManyRolls + die[i]
+        i += 1
+
+    output = ""
+    roll = 0
+
     # Checking if dice isn't too big
     if int(dice) > 100:
         await ctx.send("To big dice to roll!")
-
     else:
         # Creating sequence of values from dice to set their weights for random.choices
         seq = []
@@ -30,46 +71,64 @@ async def d(ctx, dice, howManyRolls="", modifier="", advantage=""):
 
         # Checking if second argument is empty (yes: roll only once, no: roll (howManyRolls) many times)
         if howManyRolls == "":
-            choice = random.choices(seq, weights)
-            roll = "" + str(choice[0])
-        # Rolling and suming results
-        else:
-            i = 0
-            roll = 0
-            while i < int(howManyRolls):
+            if advantageTest == 1 or advantageTest == 2:
+                if howManyRolls != "2":
+                    await ctx.send("You should roll only 2 dices when doing advantage check!")
+            else:
                 choice = random.choices(seq, weights)
                 roll = roll + choice[0]
-                i += 1
+                output = output + str(roll)
+        # Rolling and summing results
+        else:
+            if advantageTest == 1 or advantageTest == 2:
+                if howManyRolls != "2":
+                    await ctx.send("You should roll only 2 dices when doing advantage check!")
+                else:
+                    choice = random.choices(seq, weights)
+                    adv1 = choice[0]
+                    choice = random.choices(seq, weights)
+                    adv2 = choice[0]
+                    if advantageTest == 1:
+                        if adv1 > adv2:
+                            roll = roll + adv1
+                        else:
+                            roll = roll + adv2
+                    elif advantageTest == 2:
+                        if adv1 < adv2:
+                            roll = roll + adv1
+                        else:
+                            roll = roll + adv2
+                    output = "First roll: " + str(adv1) + "\nSecond roll: " + str(adv2) + "\n(" + str(roll) + ")"
+            else:
+                output = "("
+                i = 1
+                choice = random.choices(seq, weights)
+                roll = roll + choice[0]
+                output = output + str(choice[0])
+                while i < int(howManyRolls):
+                    choice = random.choices(seq, weights)
+                    roll = roll + choice[0]
+                    output = output + "+" + str(choice[0])
+                    i += 1
+                output = output + ")"
         # Adding modifier to roll
         if modifier != "":
-            if modifier[0] == "+":
-                roll = roll + int(modifier[1:])
+            if modifierTest:
+                roll = roll + int(modifier)
+                output = output + "+" + str(modifier)
             else:
-                roll = roll - int(modifier[1:])
-        # Disadvantage and andvantage roll
-        if advantage == "A":
-            choice1 = random.choices(seq, weights)
-            choice2 = random.choices(seq, weights)
-            if choice1 > choice2:
-                roll = "" + str(choice1[0])
-            elif choice2 > choice1:
-                roll = "" + str(choice2[0])
-            elif choice2 == choice1:
-                roll = "" + str(choice2[0])
+                roll = roll - int(modifier)
+                output = output + "-" + str(modifier)
 
-        elif advantage == "D":
-            choice1 = random.choices(seq, weights)
-            choice2 = random.choices(seq, weights)
-            if choice1 > choice2:
-                roll = "" + str(choice2[0])
-            elif choice2 > choice1:
-                roll = "" + str(choice1[0])
-            elif choice2 == choice1:
-                roll = "" + str(choice2[0])
-
-        embed = discord.Embed(title="Bot Roll", color=0x874efe)
-        embed.add_field(name="Your roll: ", value=roll, inline=False)
-        await ctx.send(embed=embed)
+        output = output + "=" + str(roll)
+        if (advantageTest == 1 or advantageTest == 2) and howManyRolls == "2":
+            embed = discord.Embed(title="Bot Roll", color=0x874efe)
+            embed.add_field(name="Your roll: ", value=output, inline=False)
+            await ctx.send(embed=embed)
+        elif advantageTest == 0:
+            embed = discord.Embed(title="Bot Roll", color=0x874efe)
+            embed.add_field(name="Your roll: ", value=output, inline=False)
+            await ctx.send(embed=embed)
 
 
 bot.run(TOKEN)
